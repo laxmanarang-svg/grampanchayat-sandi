6e6833ef142800fecd4f3e88f9aab9d4650d3adb
laxmanarang-svg <laxman.arang@gmail.com>
2026-07-30 03:34:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+const slider = document.getElementById("sliderImage");
+
+const images = [
+    "images/banner 1.jpeg",
+    "images/banner 2.jpeg",
+    "images/banner 3.jpeg"
+];
+
+let index = 0;
+
+function changeImage() {
+    index = (index + 1) % images.length;
+    slider.src = images[index];
+}
+
+setInterval(changeImage, 2000);
+let topButton = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+};
+
+function topFunction() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+function updateDateTime() {
+    const now = new Date();
+
+    const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
+
+    const date = now.toLocaleDateString("hi-IN", options);
+    const time = now.toLocaleTimeString("hi-IN");
+
+    document.getElementById("datetime").innerHTML =
+        "📅 " + date + " | ⏰ " + time;
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime();
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        darkModeBtn.innerHTML = "☀️ Light Mode";
+    } else {
+        darkModeBtn.innerHTML = "🌙 Dark Mode";
+    }
+});
+function searchSection() {
+
+    let value = document.getElementById("searchInput").value.toLowerCase();
+
+    if (value.includes("परिचय")) {
+        document.getElementById("parichay").scrollIntoView({behavior:"smooth"});
+    }
+    else if (value.includes("योजना")) {
+        document.getElementById("yojana").scrollIntoView({behavior:"smooth"});
+    }
+    else if (value.includes("गैलरी")) {
+        document.getElementById("gallery").scrollIntoView({behavior:"smooth"});
+    }
+    else if (value.includes("डाउनलोड")) {
+        document.getElementById("downloads").scrollIntoView({behavior:"smooth"});
+    }
+    else if (value.includes("संपर्क")) {
+        document.getElementById("contact").scrollIntoView({behavior:"smooth"});
+    }
+    else {
+        alert("कोई परिणाम नहीं मिला।");
+    }
+}
