df752fb324249012548cf516c9622d9b7eeacaa0
LAXMAN LODHI <laxman.arang@gmail.com>
2026-07-30 07:43:54 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,15 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "अपना apiKey डालें",
+  apiKey: "AIzaSyAKRFkjIYBtYeeR7FyL8nbUvdyiOsJt7Jg",
   authDomain: "gram-panchayat-sandi.firebaseapp.com",
   projectId: "gram-panchayat-sandi",
   storageBucket: "gram-panchayat-sandi.firebasestorage.app",
   messagingSenderId: "149938563786",
-  appId: "अपना appId डालें"
+  appId: "1:149938563786:web:5de4125f56f3796e55e233"
 };
 
 const app = initializeApp(firebaseConfig);
+
 const db = getFirestore(app);
 
 export { db };
