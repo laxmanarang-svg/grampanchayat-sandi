91cd44a92ef64a283b905aa78fd61a120700ac66
LAXMAN LODHI <laxman.arang@gmail.com>
2026-08-03 13:07:09 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -13,11 +13,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
-
-export { db };
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { db, auth };
